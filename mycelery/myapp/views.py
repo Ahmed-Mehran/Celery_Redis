2a4458802346c4aef166565536ce61906d19657d
Mehran Dar <mehranahmed22@gmail.com>
2026-03-27 11:52:24 +0530

    App: Setup a basic myapp to demonstrate how celery would work

---
+++ mycelery/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    
+    print("Results: ")
+    
+    return render(request, "myapp/home.html")
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
