e90e7fdfc2dbfc2ee27051109b497321b5a33b53
Mehran Dar <mehranahmed22@gmail.com>
2026-07-02 18:57:26 +0530

    Testing with .delay and a worker executing the task

--- mycelery/myapp/views.py
+++ mycelery/myapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from mycelery.celery import add
 
 # Create your views here.
 
+## Enqueue Task using delay()
 def index(request):
     
     print("Results: ")
+
+    result1 = add.delay(10, 20)
+    
+    print(result1.id)
     
     return render(request, "myapp/home.html")
 
--- mycelery/mycelery/celery.py
+++ mycelery/mycelery/celery.py
@@ -1,6 +1,6 @@
 import os
 from celery import Celery
-
+import time
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycelery.settings')
@@ -15,3 +15,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+
+@app.task
+def add(x, y):
+    
+    time.sleep(10)
+    return x+y
