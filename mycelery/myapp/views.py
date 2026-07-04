e435f0694d031cf970aaab25341d1d1c04e6b597
Mehran Dar <mehranahmed22@gmail.com>
2026-07-04 08:24:33 +0530

    tested how we can dynamically view the result executed by celery

--- mycelery/myapp/views.py
+++ mycelery/myapp/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from myapp.tasks import add
+from celery.result import AsyncResult
 
 # Create your views here.
 
 ## Enqueue Task using delay()
 def index(request):
-    
-    print("Results: ")
 
-    result1 = add.apply_async(args=[10,20])
+    result = add.apply_async(args=[10,20])
     
-    print(result1.id)
-    
-    return render(request, "myapp/home.html")
+    return render(request, "myapp/home.html", {'result':result})
 
 def about(request):
     return render(request, 'myapp/about.html')
 
 def contact(request):
     return render(request, 'myapp/contact.html')
+
+def check_result(request, task_id):
+    # Retrieve the task result using the task_id
+    result = AsyncResult(task_id)
+    # print("Ready: ", result.ready())
+    # print("Successful: ", result.successful())
+    # print("Failed: ", result.failed())
+    # print("Get: ", result.get())
+    return render(request, 'myapp/result.html', {'result':result})
